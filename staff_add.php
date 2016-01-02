a9a9ec47e79ff737b3da17703a227af19c1bf6b1
ProAkai <umigame.aj@gmail.com>
2016-01-02 01:11:39 -0600

    Create staff_add.php

---
+++ staff_add.php
@@ -0,0 +1,24 @@
+DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title> input title yo want </title>
+</head>
+<body>
+
+Add Staff<br />
+<br />
+<form method="post"action="staff_add_check.php">
+	Please input staff name.<br />
+	<input type="text" name="name" style="width:200px"><br />
+	Please input password <br />
+	<input type="password" name="pass" style="width:100px"><br />
+	Please input password again. <br />
+	<input type="password" name="pass2" style="width:100px"><br />
+	<br />
+	<input type="button" onclick="history.back()"value="back">
+	<input type="submit" value="OK">
+</form>
+
+</body>
+</html>
